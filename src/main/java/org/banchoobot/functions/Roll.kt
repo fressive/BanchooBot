4700852d7d728bc594f9fb7732ee62b54f88974c
int100 <me@int100.org>
2018-04-07 17:21:17 +0800

    roll

--- src/main/java/org/banchoobot/functions/Roll.kt
+++ src/main/java/org/banchoobot/functions/Roll.kt
@@ -3,19 +3,17 @@ package org.banchoobot.functions
 import org.banchoobot.frame.deserializer.events.Message
 import org.banchoobot.functions.annotations.CommandFunction
 import org.banchoobot.functions.interfaces.ICommandFunction
+import java.util.*
 
 /**
  * 瞎杰宝roll
  *
  * Usage:
  * roll                 返回 0 - 100 随机整数
- * roll {max}           返回 0 - {max} 随机整数
- * roll {min}-{max}     返回 {min} - {max} 随机整数
- * roll {list}          返回 {list} 内随机一个元素
  */
 @CommandFunction(command = ["roll"])
 class Roll : ICommandFunction {
     override fun onCommand(event: Message) {
-        event.reply("Reserved.")
+        event.reply(Random().let { it.setSeed(System.currentTimeMillis().hashCode().toLong()); it.nextInt(100).toString() })
     }
 }
