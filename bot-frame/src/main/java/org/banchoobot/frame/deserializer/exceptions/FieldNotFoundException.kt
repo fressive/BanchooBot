0dff63ee8d9b419680bc76c34521adad3ba7e391
int100 <me@int100.org>
2018-06-03 20:44:15 +0800

    删除无用文件

--- bot-frame/src/main/java/org/banchoobot/frame/deserializer/exceptions/FieldNotFoundException.kt
+++
@@ -1,9 +0,0 @@
-package org.banchoobot.frame.deserializer.exceptions
-
-/**
- * 字段未找到异常
- *
- * @see org.banchoobot.frame.deserializer.Deserializer
- */
-class FieldNotFoundException constructor(fieldName: String):
-        Exception("$fieldName field not found.")
